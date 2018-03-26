ed72580035ec69b89cc2d5e9617f0190ba209aed
zhengjiani <936089353@qq.com>
2018-03-26 17:27:13 +0800

    nodeprac3

---
+++ nodeprac3/server.js
@@ -0,0 +1,23 @@
+var http = require("http");
+var url = require("url");
+var formidable = require("formidable");
+
+
+
+function start(route, handle) {
+  function onRequest(request, response) {
+	
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+	
+	
+	
+		route(handle,pathname,response,postData);
+	
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
+}
+
+exports.start = start;
